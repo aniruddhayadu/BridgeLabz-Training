465a12012263cbf7f47b92969e06719165ea0511
aniruddhayadu <aniruddha9131@gmail.com>
2025-12-28 11:33:50 +0530

    [Aniruddha] : Add solution for LeetCode 169: Majority Element

---
+++ leet-code-codebase/arrays/MajorityElementLeetCode.java
@@ -0,0 +1,31 @@
+public class MajorityElementLeetCode {
+
+    // Function to find the majority element
+    public static int majorityElement(int[] nums) {
+        int count = 0;
+        int candidate = 0;
+
+        for (int num : nums) {
+            if (count == 0) {
+                candidate = num;
+            }
+
+            if (num == candidate) {
+                count++;
+            } else {
+                count--;
+            }
+        }
+
+        return candidate;
+    }
+
+    // Test the function
+    public static void main(String[] args) {
+        int[] nums = {2,2,1,1,1,2,2};
+
+        int result = majorityElement(nums);
+
+        System.out.println("Majority element: " + result);
+    }
+}
