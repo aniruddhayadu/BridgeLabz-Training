42d837f20bd8705194b703bad6c4e56b011a0afe
aniruddhayadu <aniruddha9131@gmail.com>
2025-12-31 14:41:48 +0530

    [Aniruddha] : Added Doctor class with consult() method to interact with patients

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/objectmodeling/self/Doctor.java
@@ -0,0 +1,32 @@
+package com.objectmodeling.self;
+
+import java.util.ArrayList;
+
+public class Doctor {
+    private String name;
+    private ArrayList<Patient> patients;
+
+    Doctor(String name) {
+        this.name = name;
+        patients = new ArrayList<>();
+    }
+
+    String getName() {
+        return name;
+    }
+
+    // Association: consult a patient
+    void consult(Patient patient) {
+        patients.add(patient);
+        patient.addDoctor(this);
+        System.out.println(name + " consulted with " + patient.getName());
+    }
+
+    void viewPatients() {
+        System.out.println(name + " has consulted with:");
+        for (Patient p : patients) {
+            System.out.println("- " + p.getName());
+        }
+    }
+}
+
