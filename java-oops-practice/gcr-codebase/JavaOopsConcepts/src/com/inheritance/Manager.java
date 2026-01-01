dcf0e8f498bc88b5100002e2480245f34687076f
aniruddhayadu <aniruddha9131@gmail.com>
2026-01-01 12:51:27 +0530

    [Aniruddha] : add employee hierarchy with role-specific attributes

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/inheritance/Manager.java
@@ -0,0 +1,16 @@
+package com.inheritance;
+
+public class Manager extends Employee {
+    private int teamSize;
+
+    public Manager(String name, int id, double salary, int teamSize) {
+        super(name, id, salary);
+        this.teamSize = teamSize;
+    }
+
+    @Override
+    public void displayDetails() {
+        super.displayDetails();
+        System.out.println("Team Size: " + teamSize);
+    }
+}
