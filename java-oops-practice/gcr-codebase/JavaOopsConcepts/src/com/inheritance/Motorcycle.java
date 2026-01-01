b353bc2a0ed71ce2bc4a0cef79d331417192b3d4
aniruddhayadu <aniruddha9131@gmail.com>
2026-01-01 13:07:24 +0530

    [Aniruddha] : Add vehicle hierarchy with Car, Truck, and Motorcycle

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/inheritance/Motorcycle.java
@@ -0,0 +1,15 @@
+package com.inheritance;
+public class Motorcycle extends Vehicle {
+    private boolean hasCarrier;
+
+    public Motorcycle(int maxSpeed, String fuelType, boolean hasCarrier) {
+        super(maxSpeed, fuelType);
+        this.hasCarrier = hasCarrier;
+    }
+
+    @Override
+    public void displayInfo() {
+        super.displayInfo();
+        System.out.println("Carrier Available: " + (hasCarrier ? "Yes" : "No"));
+    }
+}
