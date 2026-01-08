ae8ed234871b0e9153be4789099cd3663f581c4f
aniruddhayadu <aniruddha9131@gmail.com>
2026-01-08 11:35:42 +0530

    [Aniruddha] added code of InputStreamReader Example Example

---
+++ dsa-practice/gcr-codebase/DSAPractice/src/com/stringfileandsearch/InputStreamReaderExample.java
@@ -0,0 +1,26 @@
+package com.stringfileandsearch;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+
+public class InputStreamReaderExample {
+    public static void main(String[] args) {
+
+        try (BufferedReader br = new BufferedReader(
+                new InputStreamReader(
+                        new FileInputStream("example.txt"),
+                        StandardCharsets.UTF_8))) {
+
+            String line;
+            while ((line = br.readLine()) != null) {
+                System.out.println(line);
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
