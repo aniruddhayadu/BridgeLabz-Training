61ae6d907dbc1f9cd4d272beb7f763517c1108cc
aniruddhayadu <aniruddha9131@gmail.com>
2026-01-21 17:16:39 +0530

    [Aniruddha] : added code of added a program to compare two sets Check if Two Sets Are Equal

---
+++ java-collections-practice/gcr-code-base/JavaCollectionsConcepts/src/com/collections/set/basic/CompareSets.java
@@ -0,0 +1,29 @@
+package com.collections.set.basic;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class CompareSets {
+
+	// Method to check if two sets are equal
+	static boolean areSetsEqual(Set<Integer> set1, Set<Integer> set2) {
+
+		// equals() checks size and elements (order-independent)
+		return set1.equals(set2);
+	}
+
+	public static void main(String[] args) {
+
+		Set<Integer> set1 = new HashSet<>();
+		set1.add(1);
+		set1.add(2);
+		set1.add(3);
+
+		Set<Integer> set2 = new HashSet<>();
+		set2.add(3);
+		set2.add(2);
+		set2.add(1);
+
+		System.out.println("Are sets equal? " + areSetsEqual(set1, set2));
+	}
+}
